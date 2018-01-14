b422c3dd309a095027ee2ce1958d47e2f78ae0df
Daniel Hansson <daniel@techandme.se>
2018-01-14 23:53:21 +0100

    rename all html to php and update copyright year

---
+++ src/socialnetworks.php
@@ -0,0 +1,31 @@
+<!-- Schema.org markup for Google+ -->
+<meta itemprop="name" content="{{articleTitle}}">
+<meta itemprop="description" content="{{description}}">
+<meta itemprop="image" content="{{imageUrl}}">
+
+<!-- Twitter Card data -->
+<meta name="twitter:card" content="{{imageUrl}}">
+<meta name="twitter:site" content="@embyapp">
+<meta name="twitter:title" content="{{articleTitle}}">
+<meta name="twitter:description" content="{{description}}">
+<meta name="twitter:creator" content="@embyapp">
+<!-- Twitter summary card with large image must be at least 280x150px -->
+<meta name="twitter:image:src" content="{{largeImageUrl}}">
+
+<!-- Pinterest -->
+<meta name="p:domain_verify" content="cc2890d97a2653d9254ab08a2d1ac89f" >
+
+<!-- Open Graph data -->
+<meta property="og:title" itemprop="name" content="{{articleTitle}}" >
+<meta property="og:type" content="website" >
+<meta property="og:url" content="http://emby.media">
+<link itemprop="image" href="{{imageUrl}}">
+<meta property="og:image" content="{{imageUrl}}" >
+<meta property="og:description" itemprop="description" content="{{description}}" >
+<meta property="og:site_name" content="Emby" >
+<meta property="article:published_time" content="{{pubDate}}" >
+<meta property="article:modified_time" content="{{modifyDate}}" >
+<meta property="article:section" content="Article Section" >
+<meta property="article:tag" content="Article Tag" >
+<meta property="fb:admins" content="1618309211750238">
+<meta property="fb:app_id" content="1618309211750238">
